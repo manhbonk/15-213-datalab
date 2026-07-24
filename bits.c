bc6b7e2cfaeb83bdd3168d2bb810537d9c87a80e
Duc Manh <manhthtttb@gmail.com>
2026-07-24 16:05:21 +0700

    Solve isAsciiDigit

--- bits.c
+++ bits.c
@@ -200,7 +200,7 @@ int negate(int x) {
  *   Rating: 3
  */
 int isAsciiDigit(int x) {
-  return 2;
+  return !((x & (~(0x0f))) ^ (0x30)) & !!(  (((0x09) + (~(x & (0x0f)) + 1)) & (1 << 31)) ^ (1 << 31) );
 }
 /* 
  * conditional - same as x ? y : z 
