65436cbb5ab46710e0e9e6cd62a09f8e11c20d74
Duc Manh <manhthtttb@gmail.com>
2026-07-24 14:07:25 +0700

    Solve tmin and isTmax

--- bits.c
+++ bits.c
@@ -153,7 +153,7 @@ int bitXor(int x, int y) {
  */
 int tmin(void) {
 
-  return 2;
+  return 1 << 31;
 
 }
 //2
@@ -165,7 +165,7 @@ int tmin(void) {
  *   Rating: 1
  */
 int isTmax(int x) {
-  return 2;
+  return !((x + 1) ^ (~x)) & (!!(~x));
 }
 /* 
  * allOddBits - return 1 if all odd-numbered bits in word set to 1
